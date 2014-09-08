0087b6d0512fe971b97591cdefb8c80052e1c864
Rodney Rehm <mail+github@rodneyrehm.de>
2014-09-08 22:31:44 +0200

    more mapping

--- src/backbone.marionette.migrate.js
+++ src/backbone.marionette.migrate.js
@@ -16,19 +16,22 @@
 }(this, function(root, _, Backbone, Marionette) {
   'use strict';
   
+  /*
+    Things this Plugin does not handle:
+      * Regions need to have an element when they're showing a view. Previously you could show a view in a region and if the region didn't have an element on the page at the time, nothing would happen. Now Marionette throws an error so you know immediately that you need to fix something.
+  */
+
   proxyProperty(
     'API change: Removed the Marionette.$ proxy. We are now using Backbone.$ instead.',
     Marionette, '$', function() { return Backbone.$; }
   );
-  // Object.defineProperty(Marionette, '$', {
-  //   get: function() {
-  //     console.warn('API change: Removed the Marionette.$ proxy. We are now using Backbone.$ instead.');
-  //     return Backbone.$;
-  //   }
-  // });
-  
-  
-  
+
+  // TODO: check mapping's first character for "!", if so, it's a hint to the docs
+  // TODO: run a function signature comparison of all the mappings
+
+  // TODO: map to new signature Module.initialize(options, moduleName, app) mapped from old Module.initialize(moduleName, app, options)
+
+
   function proxyProperty(message, object, propertyName, callback) {
     Object.defineProperty(object, propertyName, {
       get: function() {
