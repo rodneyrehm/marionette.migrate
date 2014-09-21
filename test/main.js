be2946891cf75326d668d1e3a934c7c0c1dd8e5a
Rodney Rehm <mail+github@rodneyrehm.de>
2014-09-21 16:04:21 +0200

    simple tests

--- test/main.js
+++ test/main.js
@@ -20,15 +20,27 @@ require.config({
 require(['../../../src/backbone.marionette.migrate', 'backbone.marionette'], function (bridgeMarionetteMigration, Marionette) {
   bridgeMarionetteMigration(Marionette);
 
+  // API change: Removed the Marionette.$ proxy. We are now using Backbone.$ instead.
   console.log(Marionette.$('body'));
 
-  var Conti = Marionette.Controller.extend({});
+  var Conti = Marionette.Controller.extend({
+    // Marionette.Controller: the method close was renamed to destroy - both have been updated
+    close: function() {},
+    // Marionette.Controller: the event-callback onClose was renamed to onDestroy - both have been updated
+    onClose: function() {}
+  });
   var conti = new Conti();
+  // Marionette.Controller: the method close was renamed to destroy
   conti.close
 
   var View = Marionette.View.extend({});
   var view = new View();
+  // Marionette.View: the method close was renamed to destroy
   view.close
 
+  var CollectionView = Marionette.CollectionView.extend({
+    // Marionette.CollectionView: the attribute itemViewEventPrefix was renamed to childViewEventPrefix - both have been updated
+    itemViewEventPrefix: 'item',
+  });
 
 });
